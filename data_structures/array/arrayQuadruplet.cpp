9f24a4c3b455f851e72a7cb9f00927e453f02c6b
Sunil Gaikwad <sunilgaikwad@gmail.com>
2018-09-16 22:11:18 -0600

    Given an unsorted array of integers arr and a number s, write a function findArrayQuadruplet that finds four numbers (quadruplet) in arr that sum up to s. Your function should return an array of these numbers in an ascending order. If such a quadruplet doesn’t exist, return an empty array.

---
+++ data_structures/array/arrayQuadruplet.cpp
@@ -0,0 +1,102 @@
+/*Array Quadruplet
+
+Given an unsorted array of integers arr and a number s,
+write a function findArrayQuadruplet that finds four numbers (quadruplet) in arr that sum up to s.
+Your function should return an array of these numbers in an ascending order.
+If such a quadruplet doesn’t exist, return an empty array.
+
+Note that there may be more than one quadruplet in arr whose sum is s.
+You’re asked to return the first one you encounter (considering the results are sorted).
+Explain and code the most efficient solution possible, and analyze its time and space complexities.
+
+Example:
+
+input:  arr = [2, 7, 4, 0, 9, 5, 1, 3], s = 20
+
+output: [0, 4, 7, 9] # The ordered quadruplet of (7, 4, 0, 9)
+                     # whose sum is 20. Notice that there
+                     # are two other quadruplets whose sum is 20:
+                     # (7, 9, 1, 3) and (2, 4, 9, 5), but again you’re
+                     # asked to return the just one quadruplet (in an
+                     # ascending order)
+
+Constraints:
+
+    [time limit] 5000ms
+
+    [input] array.integer arr
+        1 ≤ arr.length ≤ 100
+
+    [input] integer s
+
+    [output] array.integer*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+static int cnt = 0;
+vector<int> findQuadruplet(vector<int> &input, int sum)
+{
+  size_t size = input.size();
+  if(size < 4)
+  {
+    return vector<int>();
+  }
+  sort(input.begin(), input.end());
+  if((input[size - 1] + input[size - 2] + input[size - 3] + input[size - 4]) < sum)
+    return vector<int>();
+
+  for(size_t a = 0; a <= (size - 4); a++)
+    for(size_t b = a + 1; b <= (size - 3); b++)
+    {
+        size_t c = b + 1;
+        size_t d = size - 1;
+        int tempsum = (input[a] + input[b] + input[d - 1] + input[d]);
+        if(tempsum == sum)
+        {
+          vector<int> result; result.push_back(input[a]);result.push_back(input[b]);
+          result.push_back(input[d - 1]);result.push_back(input[d]);
+          return result;
+        }
+        else if(tempsum < sum)
+          continue;
+        else
+        {
+          while(c < d)
+          {
+            cnt++;
+            tempsum = (input[a] + input[b] + input[c] + input[d]);
+            if(tempsum == sum)
+            {
+              vector<int> result; result.push_back(input[a]);result.push_back(input[b]);
+              result.push_back(input[c]);result.push_back(input[d]);
+              return result;
+            }
+            else if(tempsum < sum)
+            {
+              c++;
+            }
+            else
+            {
+              d--;
+            }
+          }
+        }
+    }
+  return vector<int>();
+}
+
+int main()
+{
+  vector<int> input{2, 7, 4, 0, 9, 5, 1, 3 };
+  int sum = 15;
+
+  vector<int> result = findQuadruplet(input, sum);
+  for(int i = 0; i < result.size(); i++)
+  {
+    cout<<result[i]<<" ";
+  }
+  cout<<endl;
+  cout<<cnt;
+}
